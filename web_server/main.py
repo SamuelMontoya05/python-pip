1ab177520d895b7b50bb3bf1f6165ae6b4f56f70
SamuelMontoya05 <samuelestebanm@gmail.com>
2023-01-24 20:09:05 -0500

    using fastapi (HTML in python)

--- web_server/main.py
+++ web_server/main.py
@@ -1,4 +1,22 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+
+app = FastAPI()
+
+
+@app.get('/')
+def get_list():
+    return [1,2,3]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_list():
+    return '''
+        <h1> Hi, I'm Samuel</h1>
+        <p>This is a test </p>
+
+'''
 
 def run():
     store.get_categories()
